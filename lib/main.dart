3ebfebda8b82e1e071db17e67a09357c9641578b
ynnuSunny <sunnysutradhar38@gmail.com>
2024-01-21 16:00:30 +0600

    firebase connection

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:user_app/authentication/login_screen.dart';
 import 'package:user_app/authentication/signup_screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: "AIzaSyAEXFnm_-Bh2IO7RfZuhtEVmi1kSXvQCbQ",
+            appId: "1:737563871705:android:a86f1ab473df9e1fd371c8",
+            messagingSenderId: "737563871705",
+            projectId: "uber-clone-583a0",
+          ),
+        )
+      : await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -15,11 +31,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: Colors.black
-      ),
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
       home: const LoginScreen(),
     );
   }
 }
-
