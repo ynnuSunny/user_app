bfe42a121f581c089b7a49fefde9262367755c3d
ynnuSunny <sunnysutradhar38@gmail.com>
2024-01-21 16:01:17 +0600

    user registration

--- lib/authentication/signup_screen.dart
+++ lib/authentication/signup_screen.dart
@@ -1,5 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:user_app/authentication/login_screen.dart';
+import 'package:user_app/methods/common_methods.dart';
+
+import '../pages/home_page.dart';
+import '../widgets/loading_dialog.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -13,6 +19,60 @@ class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController emailTextEditingController = TextEditingController();
   TextEditingController phoneTextEditingController = TextEditingController();
   TextEditingController passwordTextEditingController = TextEditingController();
+  CommonMethods cMethods = CommonMethods();
+
+  checkInternet(){
+     cMethods.checkConnectivity(context);
+     formAuthentication();
+  }
+  formAuthentication(){
+     if(userNameTextEditingController.text.trim().length<3){
+       cMethods.displaySnackBar("User name too short", context);
+     }else if(!emailTextEditingController.text.contains("@")){
+       cMethods.displaySnackBar("User email not valid", context);
+     }else if(phoneTextEditingController.text.trim().length!=11){
+       cMethods.displaySnackBar("User phone number not valid", context);
+     }else if(passwordTextEditingController.text.trim().length<5){
+       cMethods.displaySnackBar("User password too short", context);
+     }else{
+       registerNewUser();
+     }
+  }
+  registerNewUser() async
+  {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (BuildContext context) => LoadingDialog(messageText: "Registering your account..."),
+    );
+
+    final User? userFirebase = (
+        await FirebaseAuth.instance.createUserWithEmailAndPassword(
+          email: emailTextEditingController.text.trim(),
+          password: passwordTextEditingController.text.trim(),
+        ).catchError((errorMsg)
+        {
+          Navigator.pop(context);
+          cMethods.displaySnackBar(errorMsg.toString(), context);
+        })
+    ).user;
+
+    if(!context.mounted) return;
+    Navigator.pop(context);
+
+    DatabaseReference usersRef = FirebaseDatabase.instance.ref().child("users").child(userFirebase!.uid);
+    Map userDataMap =
+    {
+      "name": userNameTextEditingController.text.trim(),
+      "email": emailTextEditingController.text.trim(),
+      "phone": phoneTextEditingController.text.trim(),
+      "id": userFirebase.uid,
+      "blockStatus": "no",
+    };
+    usersRef.set(userDataMap);
+
+    Navigator.push(context, MaterialPageRoute(builder: (c)=> const HomePage()));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,7 +103,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
                           ),
 
                         ),
-                        style: TextStyle(
+                        style: const TextStyle(
                             color: Colors.grey
                         ),
                       ),
@@ -95,7 +155,9 @@ class _SignUpScreenState extends State<SignUpScreen> {
                       ),
                       const SizedBox(height: 12,),
                       ElevatedButton(
-                          onPressed: (){},
+                          onPressed: (){
+                            checkInternet();
+                          },
                           style: ElevatedButton.styleFrom(
                             backgroundColor: Colors.purple,
                             padding: const EdgeInsets.symmetric(horizontal: 80,vertical: 10),
